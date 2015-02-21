bb48afb34b6d4510eb913b5244b5634a857c063c
G Grebe <grebotics@gmail.com>
2015-02-21 15:01:09 -0500

    Added RobotComponenets - RobotComponents is a collection of parts that can be passed around representing the robot as a whole

---
+++ C/RobotOne/src/RobotComponents.h
@@ -0,0 +1,23 @@
+/*
+ * RobotComponents.h
+ *
+ *  Created on: Feb 21, 2015
+ *      Author: user
+ */
+
+#ifndef SRC_ROBOTCOMPONENTS_H_
+#define SRC_ROBOTCOMPONENTS_H_
+
+// Forward Declarations
+class RobotDrive;
+class Elevator;
+class USBCamera;
+
+struct RobotComponents
+{
+	RobotDrive*	drive;
+	Elevator*	elevator;
+	USBCamera*	camera;
+};
+
+#endif /* SRC_ROBOTCOMPONENTS_H_ */
