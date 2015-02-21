392d8ed8a6e6d29612eabbcab9ad96bba7d5fa83
G Grebe <grebotics@gmail.com>
2015-02-21 15:16:03 -0500

    Added DisabledState class

---
+++ C/RobotOne/src/DisabledState.cpp
@@ -0,0 +1,21 @@
+/*
+ * DisabledState.cpp
+ *
+ *  Created on: Feb 21, 2015
+ *      Author: user
+ */
+
+#include <DisabledState.h>
+
+namespace WPS {
+
+DisabledState::DisabledState() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DisabledState::~DisabledState() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace WPS */
---
+++ C/RobotOne/src/DisabledState.h
@@ -0,0 +1,23 @@
+/*
+ * DisabledState.h
+ *
+ *  Created on: Feb 21, 2015
+ *      Author: user
+ */
+
+#ifndef SRC_DISABLEDSTATE_H_
+#define SRC_DISABLEDSTATE_H_
+
+#include <RobotState.h>
+
+namespace WPS {
+
+class DisabledState: public RobotState {
+public:
+	DisabledState();
+	virtual ~DisabledState();
+};
+
+} /* namespace WPS */
+
+#endif /* SRC_DISABLEDSTATE_H_ */
